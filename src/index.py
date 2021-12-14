f6eb04a7ced53e9cde6b2d1d038597379b5e33b3
Sippee <sippegaming@gmail.com>
2021-12-14 18:06:09 +0200

    improve index and translated to English

--- src/index.py
+++ src/index.py
@@ -6,32 +6,32 @@ option = "0"
     
 while option != "4":
     if option=="2":
-        option=str(input("Mitä haluat tehdä? \n3: Kirjaudu ulos, \n4: Lopeta sovellus \nMitä tehdään? \n"))
+        option=str(input("What do you want to do? \n3: Logout \n4: Exit the application \nSelect your option. \n"))
     else:
-        option=str(input("Mitä haluat tehdä? \n1: Luo tunnus, \n2: Kirjaudu sisään, \n3: Kirjaudu ulos, \n4: Lopeta sovellus \nMitä tehdään? \n"))
+        option=str(input("What do you want to do? \n1: Create a new user \n2: Login \n3: Logout \n4: Exit the application \nSelect your option \n"))
 
     if option=="1":
-        name=input("Anna nimi:")
-        password=input("Anna salasana:")
+        name=input("Name:")
+        password=input("Password:")
 
         try:
             service_handler.create_user(name, password)
-            print("Loit tunnuksen")
+            print("\nYou created a user\n")
         except UserError:
             continue
 
     if option=="2":
-        name=input("Anna nimi:")
-        password=input("Anna salasana:")
+        name=input("Name:")
+        password=input("Password:")
 
         try:
             service_handler.login(name,password)
-            print("Olet kirjautunut sisään")
+            print("\nYou logged in\n")
         except UserError:
             continue
 
     if option=="3":
         service_handler.logout()
-        print("Olet kirjautunut ulos")
+        print("\nYou logged out\n")
 
 #väliaikainen
